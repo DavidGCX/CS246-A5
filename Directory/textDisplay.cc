cbc8544386a5744ef43e63b67a2ded3bec94d9fa
chuyingzhu <c63zhu@uwaterloo.ca>
2023-04-06 19:04:00 -0400

    Add simple text output for player card

--- Directory/textDisplay.cc
+++ Directory/textDisplay.cc
@@ -1,12 +1,23 @@
 #include "textDisplay.h"
 #include <memory>
 #include "player.h"
+#include <vector>
+#include <string>
+#include <iostream>
 using namespace std;
+
 // Initialize internal data
 void TextDisplay::init() {
-
+    cout << "Text Display Initializing" << endl;
 }
 
-void refresh(unique_ptr<Player>& playerOne, unique_ptr<Player>& playerTwo) {
+void TextDisplay::printToScreen() {
+    vector<string> s = display_player_card(1, "First Player", 20, 0);
+    for (auto i : s) {
+        cout << s;
+    }
+}
 
+void TextDisplay::refresh(unique_ptr<Player>& playerOne, unique_ptr<Player>& playerTwo) {
+    printToScreen();
 }
