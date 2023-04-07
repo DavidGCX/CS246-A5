7b8244f66b1a342aaf04c3025073794f40ae3101
c62gao <c62gao@uwaterloo.ca>
2023-04-06 20:12:37 -0400

    hadAbility

--- Directory/hasAbility.h
+++ Directory/hasAbility.h
@@ -11,16 +11,16 @@ public:
     int getAbilityCost() { return abilityCost; }
     int setAbilityCost(int cost) {abilityCost = cost;}
     virtual ~CanUseAbility() = 0;
-}
+};
 class HasAbility : public CanUseAbility {
     virtual bool useAbility() = 0;
 };
 
 class HasAbilityTargetMinion : public CanUseAbility {
-    virtual bool useAbility(unique_ptr<Minion>* Minion) = 0;
+    virtual bool useAbility(std::unique_ptr<Minion>* Minion) = 0;
 };
 
 class HasAbilityTargetRitual : public CanUseAbility {
-    virtual bool useAbility(unique_ptr<Ritual>* ritual) = 0;
+    virtual bool useAbility(std::unique_ptr<Ritual>* ritual) = 0;
 };
 #endif
