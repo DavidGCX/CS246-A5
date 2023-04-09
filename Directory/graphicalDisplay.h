22622e86347d8a18f8ff90242d7fb24889a54442
chuyingzhu <c63zhu@uwaterloo.ca>
2023-04-08 22:06:29 -0400

    Add XWindow to graphicalDisplay

--- Directory/graphicalDisplay.h
+++ Directory/graphicalDisplay.h
@@ -1,10 +1,15 @@
 #ifndef _GRAPHICALDISPLAY_H_
 #define _GRAPHICALDISPLAY_H_
 #include "adaptor.h"
+#include "window.h"
 #include <memory>
 class Player;
 
 class GraphicalDisplay : public Adaptor {
+    const int WIDTH = 1000;
+    const int HEIGHT = 750;
+    Xwindow* wp;
+    Xwindow w;
     public:
         GraphicalDisplay(){}
         void refresh(std::unique_ptr<Player>& playerOne, std::unique_ptr<Player>& playerTwo) override;
