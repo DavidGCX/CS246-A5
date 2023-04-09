871b3e81b9a76b580494b03972d45b8f52dd1500
Viclis <viclis@rogers.com>
2023-04-09 13:29:09 -0400

    modified:   Directory/main.cc

--- Directory/main.cc
+++ Directory/main.cc
@@ -1,83 +1,41 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
-#include "player.h"
-#include "gameController.h"
-#include "minion.h"
-#include "graphicalDisplay.h"
-#include "textDisplay.h"
+//#include "player.h"
+//#include "gameController.h"
+//#include "minion.h"
+//#include "graphicalDisplay.h"
+//#include "textDisplay.h"
+#include <string>
 
 using namespace std;
 
-int main(int argc, const char** argv) {
-    bool graphicson = false;
-    bool testmode = false;
-    string deck1 = "default.deck";
-    string deck2 = "default.deck";
-    unique_ptr<GameController> gc;
-    string initfile;
-    bool init = false;
-    for (int i = 1; i < argc; ++i) {
-        if (argv[i] == "-deck1") {
-            deck1 = argv[i+1];
-            ++i;
-        } else if (argv[i] == "-deck2") {
-            deck2 = argv[i+1];
-            ++i;
-        } else if (argv[i] == "-init") {
-            init = true;
-            initfile = argv[i+1];
-            ++i;
-        } else if (argv[i] == "-testing") {
-            testmode = true;
-            gc->setTestMode();
-        } else if (argv[i] == "-graphics") {
-            bool graphicson = true;
-            gc->attachAdapter(make_unique<GraphicalDisplay>());
-            gc->attachAdapter(make_unique<TextDisplay>());
-        }
-    }
-    if (graphicson == false) {
-        gc->attachAdapter(make_unique<TextDisplay>());
-    }
-    if (init == true) {
-        fstream f{initfile};
-    }
-    string cmd;
-    bool hasFirstPlayer = false;
-    bool hasSecondPlayer = false;
-
-    while(true) {
-        if(init == true) {
-            getline(f, cmd);
-            istringstream s{cmd};
-            if (f.eof()) {
-                init = false;
-            }
-        } else {
-            getline(cin, cmd);  
-            istringstream s{cmd};        
-        }
+bool whileloop (istream& s, bool testmode, bool &hasFirstPlayer, bool &hasSecondPlayer) {
+	bool value = false;
+	string firstword;
+	s >> firstword;
         if (!hasFirstPlayer) {
-            gc->attachPlayer(cmd, 1, deck1);
+            //gc->attachPlayer(cmd, 1, deck1);
             hasFirstPlayer = true;
-        }
-        else if (!hasSecondPlayer) {
-            gc->attachPlayer(cmd, 2, deck2);
+	    cout << "first player done" << endl;
+        } else if (!hasSecondPlayer) {
+            //gc->attachPlayer(cmd, 2, deck2);
             hasSecondPlayer = true;
-        }
-        else {
-            if (s == "draw") {
-                if (testmode) gc->drawCard();
-            }
-            else if (s == "discard") {
+	    cout << "second player done" << endl;
+        } else {
+            if (firstword == "draw") {
+                if (testmode) {
+			//gc->drawCard();
+			cout << "draw" << endl;
+		}
+            } else if (firstword == "discard") {
                 if (testmode) {
                     int num;
                     s >> num;
-                    gc->discardCard(num);
+                    //gc->discardCard(num);
+		    cout << "discard" << endl; 
                 }
-            }
-            else if (s == "help") {
+            } else if (firstword == "help") {
                 cout << "Commands: help -- Display this message." << endl;
                 cout << "          end -- End the current player’s turn." << endl;
                 cout << "          quit -- End the game." << endl;
@@ -88,136 +46,132 @@ int main(int argc, const char** argv) {
                 cout << "          inspect minion -- View a minion’s card and all enchantments on that minion." << endl;
                 cout << "          hand -- Describe all cards in your hand." << endl;
                 cout << "          board -- Describe all cards on the board." << endl;
-            }
-            else if (s == "end") {
-                gc->endTurn();
-            }
-            else if (s == "quit") {
-                break;
-            }
-            else if (s == "attack") {
+            } else if (firstword == "end") {
+                //gc->endTurn();
+		cout << "end" << endl;
+            } else if (firstword == "quit") {
+		cout << "quit" << endl;
+                value = true;
+            } else if (firstword == "attack") {
                 int attacker;
                 int target;
                 s >> attacker;
                 if (!s.eof()) {
                     s >> target;
-                    gc->attack(attacker, target);
+                    //gc->attack(attacker, target);
+		    cout << "attack 2 params" << endl;
                 } else {
-                    gc->attack(attacker);
+                    //gc->attack(attacker);
+		    cout << "attack 1 param" << endl;
                 }
-            } else if (s == "play") {
+                //gc->attack(attacker, target);
+            } else if (firstword == "play") {
                 int card;
                 s >> card;
                 if (s.eof()) {
-                    gc->play(card);
+                    //gc->play(card);
+		    cout << "play 1 param" << endl;
                 } else {
                     char target;
                     int player;
                     s >> player;
                     s >> target;
-                    gc->play(card, player, target);
+                    //gc->play(card, player, target);
+		    cout << "play 3 params" << endl;
                 }
-            }  else if (s == "use") {
+            } else if (firstword == "use") {
                 int card;
                 s >> card;
                 if (s.eof()) {
-                    gc->use(card);
+                    //gc->use(card);
+		    cout << "use 1 param" << endl;
                 } else {
                     char target;
                     int player;
                     s >> target;
                     s >> player;
-                    gc->use(card,  player, target);
+                    //gc->use(card,  player, target);
+		    cout << "use 3 params" << endl;
                 }
-            } else if (s == "inspect") {
+            } else if (firstword == "inspect") {
                 int minion;
                 s >> minion;
-                gc->inspect(minion);
-            } else if (s == "board") {
-                gc->board();
-            } else if (s == "hand") {
-                gc->hand();
+                //gc->inspect(minion);
+		cout << "inspect" << endl;
+            } else if (firstword == "board") {
+                //gc->board();
+		cout << "board" << endl;
+            } else if (firstword == "hand") {
+                //gc->hand();
+		cout << "hand" << endl;
             }
+	}
+	return value;
+}
+
+int main(int argc, const char* argv[]) {
+    bool graphicson = false;
+    bool testmode = false;
+    string deck1 = "default.deck";
+    string deck2 = "default.deck";
+    //unique_ptr<GameController> gc;
+    string initfile;
+    bool init = false;
+    for (int i = 1; i < argc; ++i) {
+	string arg1 = argv[i];
+        if (arg1 == "-deck1") {
+            deck1 = argv[i+1];
+            ++i;
+	    cout << "-deck1" << endl;
+        } else if (arg1 == "-deck2") {
+            deck2 = argv[i+1];
+            ++i;
+	    cout << "-deck2" << endl;
+        } else if (arg1 == "-init") {
+            init = true;
+            initfile = argv[i+1];
+            ++i;
+	    cout << "-init" << endl;
+        } else if (arg1 == "-testing") {
+            testmode = true;
+            //gc->setTestMode();
+	    cout << "-testing" << endl;
+        } else if (arg1 == "-graphics") {
+            bool graphicson = true;
+            //gc->attachAdapter(make_unique<GraphicalDisplay>());
+            //gc->attachAdapter(make_unique<TextDisplay>());
+	    cout << "-graphics" << endl;
         }
-        // if (count == 1) {
-        //     gc->attachPlayer(line, 1, deck1);
-        //     ++count;
-        //     continue;
-        // } else if (count == 2) {
-        //     gc->attachPlayer(line, 2, deck2);
-        //     ++count;
-        //     continue;
-        // } else {
-        //     istringstream s{line};
-        //     string firstword;
-        //     s >> firstword;
-            // if (testmode == true) {
-            //     if (firstword == "draw") {
-            //         gc->drawCard();
-            //     } else if (firstword == "discard") {
-            //         int num;
-            //         s >> num;
-            //         gc->discardCard(num);
-            //     }
-            // }
-            // if (firstword == "help") {
-            //     cout << "Commands: help -- Display this message." << endl;
-            //     cout << "          end -- End the current player’s turn." << endl;
-            //     cout << "          quit -- End the game." << endl;
-            //     cout << "          attack minion other-minion -- Orders minion to attack other-minion." << endl;
-            //     cout << "          attack minion -- Orders minion to attack the opponent." << endl;
-            //     cout << "          play card [target-player target-card] -- Play card, optionally targeting target-card owned by target-player." << endl;
-            //     cout << "          use minion [target-player target-card] -- Use minion’s special ability, optionally targeting target-card owned by target-player." << endl;
-            //     cout << "          inspect minion -- View a minion’s card and all enchantments on that minion." << endl;
-            //     cout << "          hand -- Describe all cards in your hand." << endl;
-            //     cout << "          board -- Describe all cards on the board." << endl;
-            // } else if (firstword == "end") {
-            //     gc->endTurn();
-            // } else if (firstword == "quit") {
-            //     break;
-            // } else if (firstword == "attack") {
-            //     int attacker;
-            //     int target;
-            //     s >> attacker;
-            //     if (!s.eof()) {
-            //         s >> target;
-            //         gc->attack(attacker, target);
-            //     } else {
-            //         gc->attack(attacker);
-            //     }
-            // } else if (firstword == "play") {
-            //     int card;
-            //     s >> card;
-            //     if (s.eof()) {
-            //         gc->play(card);
-            //     } else {
-            //         char target;
-            //         int player;
-            //         s >> player;
-            //         s >> target;
-            //         gc->play(card, player, target);
-            //     }
-            // } else if (firstword == "use") {
-            //     int card;
-            //     s >> card;
-            //     if (s.eof()) {
-            //         gc->use(card);
-            //     } else {
-            //         char target;
-            //         int player;
-            //         s >> target;
-            //         s >> player;
-            //         gc->use(card,  player, target);
-            //     }
-            // } else if (firstword == "inspect") {
-            //     int minion;
-            //     s >> minion;
-            //     gc->inspect(minion);
-            // } else if (firstword == "board") {
-            //     gc->board();
-            // } else if (firstword == "hand") {
-            //     gc->hand();
-            // }
+    }
+    if (graphicson == false) {
+        //gc->attachAdapter(make_unique<TextDisplay>());
+    }
+    fstream f{initfile};
+    string cmd;
+    bool hasFirstPlayer = false;
+    bool hasSecondPlayer = false;
+    bool quitting = false;
+    while(true) {
+    	if(init == true) {
+            getline(f, cmd);
+            istringstream s{cmd};
+            if (f.eof()) {
+                init = false;
+            }
+	    cout << "we on file" << endl;
+	    quitting = whileloop(s, testmode, hasFirstPlayer, hasSecondPlayer);
+	    if (quitting == true) {
+		    break;
+            }
+    	} else {
+            getline(cin, cmd);
+            istringstream s{cmd};
+	    cout << "we on std input" << endl;
+	    quitting = whileloop(s, testmode, hasFirstPlayer, hasSecondPlayer);
+	    if (quitting == true) {
+		    break;
+            }
+    	}
     }
     return 0;
 }
