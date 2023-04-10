6744b569e601c3e61e26ec06e79ef59d1c8874e7
c62gao <c62gao@uwaterloo.ca>
2023-04-09 23:03:12 -0400

    fixed graphical display

--- Directory/graphicalDisplay.h
+++ Directory/graphicalDisplay.h
@@ -10,34 +10,34 @@ class GraphicalDisplay : public Adaptor {
     const int CARD_W = 200;
     const int CARD_H = 125;
     const int BORDER_W = 25;
-    const std::pair<int, int> p1ritual (25, 25);
-    const std::pair<int, int> p1name (475, 25);
-    const std::pair<int, int> p1grave (925, 25);
-    const std::pair<int, int> p1m1 (25, 175);
-    const std::pair<int, int> p1m2 (250, 175);
-    const std::pair<int, int> p1m3 (475, 175);
-    const std::pair<int, int> p1m4 (700, 175);
-    const std::pair<int, int> p1m5 (925, 175);
+    const std::pair<int, int> p1ritual = make_pair(25, 25);
+    const std::pair<int, int> p1name = make_pair(475, 25);
+    const std::pair<int, int> p1grave = make_pair(925, 25);
+    const std::pair<int, int> p1m1 = make_pair(25, 175);
+    const std::pair<int, int> p1m2 = make_pair(250, 175);
+    const std::pair<int, int> p1m3 = make_pair(475, 175);
+    const std::pair<int, int> p1m4 = make_pair(700, 175);
+    const std::pair<int, int> p1m5 = make_pair(925, 175);
 
-    const std::pair<int, int> p2m1 (25, 340);
-    const std::pair<int, int> p2m2 (250, 340);
-    const std::pair<int, int> p2m3 (475, 340);
-    const std::pair<int, int> p2m4 (700, 340);
-    const std::pair<int, int> p2m5 (925, 340);
-    const std::pair<int, int> p2ritual (25, 490);
-    const std::pair<int, int> p2name (475, 490);
-    const std::pair<int, int> p2grave (925, 490);
+    const std::pair<int, int> p2m1 = make_pair(25, 340);
+    const std::pair<int, int> p2m2 = make_pair(250, 340);
+    const std::pair<int, int> p2m3 = make_pair(475, 340);
+    const std::pair<int, int> p2m4 = make_pair(700, 340);
+    const std::pair<int, int> p2m5 = make_pair(925, 340);
+    const std::pair<int, int> p2ritual = make_pair(25, 490);
+    const std::pair<int, int> p2name = make_pair(475, 490);
+    const std::pair<int, int> p2grave = make_pair(925, 490);
 
-    const std::pair<int, int> hand1 (25, 650);
-    const std::pair<int, int> hand2 (250, 650);
-    const std::pair<int, int> hand3 (475, 650);
-    const std::pair<int, int> hand4 (700, 650);
-    const std::pair<int, int> hand5 (925, 650);
+    const std::pair<int, int> hand1 = make_pair(25, 650);
+    const std::pair<int, int> hand2 = make_pair(250, 650);
+    const std::pair<int, int> hand3 = make_pair(475, 650);
+    const std::pair<int, int> hand4 = make_pair(700, 650);
+    const std::pair<int, int> hand5 = make_pair(925, 650);
 
     const int WIDTH = 1250;
     const int HEIGHT = 800;
     Xwindow* wp;
-    Xwindow w(WIDTH, HEIGHT);
+    Xwindow w{WIDTH, HEIGHT};
 
     std::pair<int,int> drawName(const std::pair<int,int> coord);
     std::pair<int,int> drawBottomLeft(const std::pair<int,int> coord);
