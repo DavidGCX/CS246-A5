18d0c6a3413237a587ac8c5a0b74ef63d1560eff
chuyingzhu <c63zhu@uwaterloo.ca>
2023-04-09 21:20:58 -0400

    Add graphics outline

--- Directory/graphicalDisplay.cc
+++ Directory/graphicalDisplay.cc
@@ -3,6 +3,69 @@
 #include "player.h"
 using namespace std;
 
-void refresh(unique_ptr<Player>& playerOne, unique_ptr<Player>& playerTwo) {
+GraphicalDisplay::GraphicalDisplay(): wp{&w} {
+    wp->drawString(565, 325, "SORCERY");
+}
 
+pair<int,int> GraphicalDisplay::drawName(const pair<int,int> coord) {
+    pair<int,int> ans;
+    ans.first = coord.first + 10;
+    ans.second = coord.second + 15;
+    return ans;
+}
+
+pair<int,int> GraphicalDisplay::drawBottomLeft(const pair<int,int> coord) {
+    pair<int,int> ans;
+    ans.first = coord.first + 10;
+    ans.second = coord.second + 120;
+    return ans;
+}
+
+pair<int,int> GraphicalDisplay::drawBottomRight(const pair<int,int> coord) {
+    pair<int,int> ans;
+    ans.first = coord.first + 190;
+    ans.second = coord.second + 120;
+    return ans;
+}
+
+void GraphicalDisplay::refresh(unique_ptr<Player>& playerOne, unique_ptr<Player>& playerTwo) {
+    wp->fillRectangle(p1ritual.first, p1ritual.second, CARD_W, CARD_H, Xwindow::Red);
+
+    wp->fillRectangle(p1name.first, p1name.second, CARD_W, CARD_H, Xwindow::Red);
+    wp->drawString(drawName(p1name).first, drawName(p1name).second, playerOne->getName());
+    wp->drawString(drawBottomLeft(p1name).first, drawBottomLeft(p1name).second, to_string(playerOne->getHealth()));
+    wp->drawString(drawBottomRight(p1name).first, drawBottomRight(p1name).second, to_string(playerOne->getMagic()));
+
+    wp->fillRectangle(p1grave.first, p1grave.second, CARD_W, CARD_H, Xwindow::Red);
+
+    wp->fillRectangle(p1m1.first, p1m1.second, CARD_W, CARD_H, Xwindow::Red);
+    wp->fillRectangle(p1m2.first, p1m2.second, CARD_W, CARD_H, Xwindow::Red);
+    wp->fillRectangle(p1m3.first, p1m3.second, CARD_W, CARD_H, Xwindow::Red);
+    wp->fillRectangle(p1m4.first, p1m4.second, CARD_W, CARD_H, Xwindow::Red);
+    wp->fillRectangle(p1m5.first, p1m5.second, CARD_W, CARD_H, Xwindow::Red);
+
+    wp->fillRectangle(p2m1.first, p2m1.second, CARD_W, CARD_H, Xwindow::Blue);
+    wp->fillRectangle(p2m2.first, p2m2.second, CARD_W, CARD_H, Xwindow::Blue);
+    wp->fillRectangle(p2m3.first, p2m3.second, CARD_W, CARD_H, Xwindow::Blue);
+    wp->fillRectangle(p2m4.first, p2m4.second, CARD_W, CARD_H, Xwindow::Blue);
+    wp->fillRectangle(p2m5.first, p2m5.second, CARD_W, CARD_H, Xwindow::Blue);
+
+    wp->fillRectangle(p2ritual.first, p2ritual.second, CARD_W, CARD_H, Xwindow::Blue);
+
+    wp->fillRectangle(p2name.first, p2name.second, CARD_W, CARD_H, Xwindow::Blue);
+    wp->drawString(drawName(p2name).first, drawName(p2name).second, playerTwo->getName());
+    wp->drawString(drawBottomLeft(p2name).first, drawBottomLeft(p2name).second, to_string(playerTwo->getHealth()));
+    wp->drawString(drawBottomRight(p2name).first, drawBottomRight(p2name).second, to_string(playerTwo->getMagic()));
+
+    wp->fillRectangle(p2grave.first, p2grave.second, CARD_W, CARD_H, Xwindow::Blue);
+
+    wp->drawString(25, 640, "Active Player Hand:");
+
+    wp->fillRectangle(hand1.first, hand1.second, CARD_W, CARD_H, Xwindow::Green);
+    wp->fillRectangle(hand2.first, hand2.second, CARD_W, CARD_H, Xwindow::Green);
+    wp->fillRectangle(hand3.first, hand3.second, CARD_W, CARD_H, Xwindow::Green);
+    wp->fillRectangle(hand4.first, hand4.second, CARD_W, CARD_H, Xwindow::Green);
+    wp->fillRectangle(hand5.first, hand5.second, CARD_W, CARD_H, Xwindow::Green);
+
+    // active player hand
 }
--- Directory/graphicalDisplay.h
+++ Directory/graphicalDisplay.h
@@ -3,13 +3,46 @@
 #include "adaptor.h"
 #include "window.h"
 #include <memory>
+#include <utility>
 class Player;
 
 class GraphicalDisplay : public Adaptor {
-    const int WIDTH = 1000;
-    const int HEIGHT = 750;
+    const int CARD_W = 200;
+    const int CARD_H = 125;
+    const int BORDER_W = 25;
+    const std::pair<int, int> p1ritual (25, 25);
+    const std::pair<int, int> p1name (475, 25);
+    const std::pair<int, int> p1grave (925, 25);
+    const std::pair<int, int> p1m1 (25, 175);
+    const std::pair<int, int> p1m2 (250, 175);
+    const std::pair<int, int> p1m3 (475, 175);
+    const std::pair<int, int> p1m4 (700, 175);
+    const std::pair<int, int> p1m5 (925, 175);
+
+    const std::pair<int, int> p2m1 (25, 340);
+    const std::pair<int, int> p2m2 (250, 340);
+    const std::pair<int, int> p2m3 (475, 340);
+    const std::pair<int, int> p2m4 (700, 340);
+    const std::pair<int, int> p2m5 (925, 340);
+    const std::pair<int, int> p2ritual (25, 490);
+    const std::pair<int, int> p2name (475, 490);
+    const std::pair<int, int> p2grave (925, 490);
+
+    const std::pair<int, int> hand1 (25, 650);
+    const std::pair<int, int> hand2 (250, 650);
+    const std::pair<int, int> hand3 (475, 650);
+    const std::pair<int, int> hand4 (700, 650);
+    const std::pair<int, int> hand5 (925, 650);
+
+    const int WIDTH = 1250;
+    const int HEIGHT = 800;
     Xwindow* wp;
-    Xwindow w;
+    Xwindow w(WIDTH, HEIGHT);
+
+    std::pair<int,int> drawName(const std::pair<int,int> coord);
+    std::pair<int,int> drawBottomLeft(const std::pair<int,int> coord);
+    std::pair<int,int> drawBottomRight(const std::pair<int,int> coord);
+
     public:
         GraphicalDisplay(){}
         void refresh(std::unique_ptr<Player>& playerOne, std::unique_ptr<Player>& playerTwo) override;
